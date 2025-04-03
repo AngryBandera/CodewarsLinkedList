4debb3af512ee815e5989198324ca26a004b97e7
AngryBandera <52661805+AngryBandera@users.noreply.github.com>
2025-04-03 09:27:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,8 @@ class Node:
 '''
     
 def push(head, data):
-    if head is None:
-        return Node(data)
-    node = Node(data)
-    node.next = head
-    return node
-
+    return Node(data, head)
+  
 def build_one_two_three():
-    head = Node(3)
-    for i in range(2, 0, -1):
-        node = Node(i)
-        node.next = head
-        head = node
-        
-    return head
+    return Node(1, Node(2, Node(3)))
         
