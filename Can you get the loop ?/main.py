4487f67389c0574ec61dc804c6f49f6aa2882d8f
AngryBandera <52661805+AngryBandera@users.noreply.github.com>
2025-04-04 20:32:25 +0300

    Made it work!

--- Can you get the loop ?/main.py
+++ Can you get the loop ?/main.py
@@ -1,27 +1,19 @@
 def loop_size(node):
-    head = node.next
-    traversed_nodes = node
-    i = 0
-    while True:
-        if is_in_traversed(traversed_nodes, head, i):
-            return i
-        traversed_nodes.next = head
-        head = head.next
-        i += 1
-        
+        slow = node
+        fast = node
 
-def is_in_traversed(traversed, node, i):
-    head = traversed
-    if node is traversed:
-        print(1)
-        return True
-    i_ = 1
-    while head.next is not None and i_ < i:
-        i_ += 1
-        print(str(i_) + " ##")
-        if node == head.next:
-            print('ended')
-            return True
-        head = head.next
-    
-    return False
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else:
+            return 0
+
+        count = 1
+        current = slow.next
+        while current != slow:
+            count += 1
+            current = current.next
+
+        return count
